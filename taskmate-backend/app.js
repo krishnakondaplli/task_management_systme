fd61bd8c590cec3a927fc20f4c42ad9967a3e9e9
Your Name <you@example.com>
2025-05-08 23:57:22 +0530

    updated

--- taskmate-backend/app.js
+++ taskmate-backend/app.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin:
+      "http://localhost:https://task-management-system-flame-theta.vercel.app/",
     credentials: true,
   })
 );
